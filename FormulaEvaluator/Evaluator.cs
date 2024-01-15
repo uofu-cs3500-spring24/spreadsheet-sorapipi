933e532a973b8fa2e0cdf851989c3282c00f3c34
Yinghao Chen <u1376936@umail.utah.edu>
2024-01-15 00:17:39 -0700

    Created the basic Evaluator class with header comments and the main required function

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,24 @@
+﻿namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Author:    YINGHAO CHEN
+    /// Partner:   -NONE-
+    /// Date:      15/01/2024
+    /// Course:    CS 3500, University of Utah, School of Computing
+    /// Copyright: CS 3500 and YINGHAO CHEN - This work may not 
+    ///            be copied for use in Academic Coursework.
+    ///
+    /// I, YINGHAO CHEN, certify that I wrote this code from scratch and
+    /// did not copy it in part or whole from another source.  All 
+    /// references used in the completion of the assignments are cited 
+    /// in my README file.
+    ///
+    /// File Contents
+    ///
+    ///    This library class evaluates ...
+    /// </summary>
+    public class Evaluator
+    {
+
+    }
+}
